4e3667f279021ea3d98d5ed049b08193588e4e0e
Levente Lengyel <100836795+Stack0verflow@users.noreply.github.com>
2023-10-21 15:08:15 +0200

    Creating the base project
    Creating the base ASP.NET Core MVC project (.NET 7) in JetBrains Rider

---
+++ Szakdolgozat_Lengyel_Levente_BSc/Szakdolgozat_Lengyel_Levente_BSc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Szakdolgozat_Lengyel_Levente_BSc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
