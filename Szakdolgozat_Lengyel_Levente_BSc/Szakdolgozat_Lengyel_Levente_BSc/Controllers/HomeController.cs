f795e2d48456dc9962e80a3f9fdf57d164f28a05
Levente Lengyel <100836795+Stack0verflow@users.noreply.github.com>
2023-10-29 10:04:09 +0100

    Basic page changes
    Changing Page1 into Database page and adding some text and links on the main page

--- Szakdolgozat_Lengyel_Levente_BSc/Szakdolgozat_Lengyel_Levente_BSc/Controllers/HomeController.cs
+++ Szakdolgozat_Lengyel_Levente_BSc/Szakdolgozat_Lengyel_Levente_BSc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Page1()
+    public IActionResult Database()
     {
         return View();
     }
