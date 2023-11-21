0a84568eef51c6772ac88a265eb3cee48984ee6d
Levente Lengyel <100836795+Stack0verflow@users.noreply.github.com>
2023-11-21 20:11:07 +0100

    Started examiner app
    Getting data from the server through the examiner app (writes the http response to the console) - only works with given parameters yet

--- Szakdolgozat_Lengyel_Levente_BSc/Vizsgalo/Program.cs
+++ Szakdolgozat_Lengyel_Levente_BSc/Vizsgalo/Program.cs
@@ -4,16 +4,60 @@ namespace Vizsgalo
 {
     public class Scanner
     {
-        // private static string url = "http://http://localhost:5227/Users/Sqlserver";
-        private static string _url = "http://localhost:5227/Users/Sqlserver?id=3&password=akivagyok92";
-
-        public static void Main()
+        private static string _getDatabase = "";
+        private static string _urlSqlServer = "Sqlserver?id=3&password=akivagyok92";
+        private static string _urlMysql = "Mysql?id=3&password=akivagyok92";
+        
+        private static HttpClient _httpClient = new()
         {
-            HttpClient httpClient = new HttpClient();
-            Task<HttpResponseMessage> httpResponse = httpClient.GetAsync(_url);
-            HttpResponseMessage httpResponseMessage = httpResponse.Result;
-            Console.WriteLine(httpResponseMessage.Content);
-            httpClient.Dispose();
+            BaseAddress = new Uri("http://localhost:5227/Users/")
+        };
+
+        public static async Task Main()
+        {
+            Console.WriteLine("Examination started");
+            
+            //sends a GET request to the given url and writes the response to the console
+            await GetAsync(_httpClient);
+            
+            Console.WriteLine("Examination finished");
+        }
+        
+        //sends a GET request to the given url and writes the response to the console
+        static async Task GetAsync(HttpClient httpClient)
+        {
+            //SQL Server - GET
+            await SqlServerGet(httpClient);
+
+            //MySQL - GET
+            // await MySqlGet(httpClient);
+        }
+
+        static async Task SqlServerGet(HttpClient httpClient)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync(_urlSqlServer);
+            
+            Console.WriteLine(response.RequestMessage);
+            
+            // response.EnsureSuccessStatusCode()
+            //     .WriteRequestToConsole();
+    
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
+        }
+        
+        
+        static async Task MySqlGet(HttpClient httpClient)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync(_urlMysql);
+            
+            Console.WriteLine(response.RequestMessage);
+            
+            // response.EnsureSuccessStatusCode()
+            //     .WriteRequestToConsole();
+    
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"{jsonResponse}\n");
         }
     }
 }
